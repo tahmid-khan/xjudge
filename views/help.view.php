ef4dca55ff7dc9a96e4c7529f5473d296b61d91f
Tahmid A. Khan <tahmidkhan@proton.me>
2023-10-22 11:23:58 +0600

    Add some help page content

--- views/help.view.php
+++ views/help.view.php
@@ -4,7 +4,11 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>Xjudge is a virtual judge system and contest hosting platform for algorithmic programming contests.</p>
+        <h2 class="text-2xl font-bold tracking-tight text-gray-900 mb-4">Adding problems</h2>
+        <p>
+            Use this format for specifying Codeforces problems: ‘〈<span class="italic">contest ID</span>〉<code class="font-mono">/</code>〈<span class="italic">problem index</span>〉’.
+            For example, <code class="font-mono">44/A</code>.
+        </p>
     </div>
 </main>
 
