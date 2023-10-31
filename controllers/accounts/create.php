5a0bd1ebc6d89ea1fcaf7afbc8d8b777d5114c1d
Tahmid A. Khan <tahmidkhan@proton.me>
2023-10-31 13:56:00 +0600

    Fix sign in with Google

--- controllers/accounts/create.php
+++ controllers/accounts/create.php
@@ -1,3 +1,11 @@
 <?php
 
+$client = new Google_Client();
+$client->setClientId('312683763511-ll5e5svm78d9bqaai76qt620fk1hom1d.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('https://osprey-good-manually.ngrok-free.app/callback.php');
+$client->addScope("email");
+$client->addScope("profile");
+
+$loginUrl = $client->createAuthUrl();
 require BASE_PATH . 'views/accounts/create.view.php';
--- controllers/sessions/create.php
+++ controllers/sessions/create.php
@@ -1,4 +1,12 @@
 <?php
 
+$client = new Google_Client();
+$client->setClientId('312683763511-ll5e5svm78d9bqaai76qt620fk1hom1d.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('https://osprey-good-manually.ngrok-free.app/callback.php');
+$client->addScope("email");
+$client->addScope("profile");
+
 $auth_error = $_SESSION['auth_error'] ?? null;
+$loginUrl = $client->createAuthUrl();
 require BASE_PATH . 'views/sessions/create.view.php';
